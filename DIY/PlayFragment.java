1b7b3d24a1c9040831efa7549793f5eeaeab1245
tyuug <113109041+yunboxtv@users.noreply.github.com>
2022-12-02 19:02:58 +0800

    Add files via upload
    Signed-off-by: tyuug <113109041+yunboxtv@users.noreply.github.com>

--- DIY/PlayFragment.java
+++ DIY/PlayFragment.java
@@ -715,10 +715,10 @@ public class PlayFragment extends BaseLazyFragment {
                 mVodPlayerCfg.put("sp", 1.0f);
             }
             if (!mVodPlayerCfg.has("st")) {
-                mVodPlayerCfg.put("st", 0);
+                mVodPlayerCfg.put("st", 105);
             }
             if (!mVodPlayerCfg.has("et")) {
-                mVodPlayerCfg.put("et", 0);
+                mVodPlayerCfg.put("et", 120);
             }
         } catch (Throwable th) {
 
